e22a354842553e8c0af760fdb3e7814b86efc059
PTD-Data-Team <schumen@wsdot.wa.gov>
2021-01-15 13:09:33 -0800

    had some issues

--- venv/scrapers/ntdfiledownloader.py
+++ venv/scrapers/ntdfiledownloader.py
@@ -1,15 +1,39 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException, NoSuchWindowException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException, NoSuchWindowException, TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import pandas as pd
 import time
 import random
 import csv
 
-driver = webdriver.Chrome(r'C:\Users\SchumeN\Documents\webscraping\chromedriver.exe')
-df = pd.read_csv(r'C:\Users\SchumeN\Documents\databaseproject\ntddata\links.csv')
-for i in df.links.tolist():
-    driver.get(i)
-    file = driver.find_element_by_xpath('//*[@id="content-article"]/div[1]/div/div/span/a')
-    file.click()
+
+
+
+def find_link(tag_name):
+    delay = 30
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, tag_name))
+        WebDriverWait(driver, delay).until(element_present)
+        driver.find_element_by_xpath(tag_name).click()
+    except TimeoutException:
+        print("Timed out waiting for page to load")
+
+
+
+driver = webdriver.Chrome(r'C:\Users\SchumeN\Documents\Old Stuff\webscraping\chromedriver.exe')
+driver.get('https://faces.fta.dot.gov/suite/')
+driver.find_element_by_xpath('//*[@id="notification"]/div[2]/div/div/input').click()
+elem = driver.find_element_by_name('un')
+elem.send_keys('schumen@wsdot.wa.gov')
+elem2 = driver.find_element_by_name('pw')
+elem2.send_keys('Mroshhashanah4:1')
+driver.find_element_by_xpath('//*[@id="loginForm"]/div[3]/div/div[2]/input').click()
+find_link('//*[@id="sitesBody"]/div/div/div/div/div/div[4]/div/div[1]/div/div[1]/div[1]/div/div/div[1]/div[2]/div/div[2]/div/div[2]/div/p')
+
+
+
+
